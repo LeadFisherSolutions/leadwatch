2e7b63da67af8d354f9c773d87bbbe3eacd27f31
sashapop10 <sashapop10@yandex.ru>
2023-04-13 19:19:27 +0300

    Node prefix

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const { DirectoryWatcher } = require('..');
 const test = require('node:test');
 const assert = require('node:assert');
