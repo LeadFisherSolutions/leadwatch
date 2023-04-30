963b58ffc184a649ddf682ea1be5f62ea1af6018
sashapop10 <sashapop10@yandex.ru>
2023-04-30 18:25:54 +0300

    Node 20

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ class DirectoryWatcher extends EventEmitter {
 
   #setWatcher = targetPath => {
     if (this.#watchers.has(targetPath)) return;
-    const watcher = fs.watch(targetPath, (event, filename) => {
+
+    const watcher = fs.watch(targetPath, (_, filename) => {
       const target = targetPath.endsWith(path.sep + filename);
       const filePath = target ? targetPath : path.join(targetPath, filename);
       if (!this.#access(filePath)) return;
@@ -72,18 +73,16 @@ class DirectoryWatcher extends EventEmitter {
 
     fs.stat(targetPath, (err, stats) => {
       if (err) return;
-
-      if (stats.isDirectory())
-        fs.readdir(targetPath, { withFileTypes: true }, (err, files) => {
-          if (err) return;
-          for (const file of files) {
-            if (!this.#access(file.name) || !file.isDirectory() || !this.#options.deep) continue;
-            const dirPath = path.join(targetPath, file.name);
-            this.watch(dirPath);
-          }
-        });
-
       this.#setWatcher(targetPath);
+
+      if (!stats.isDirectory()) return;
+      fs.readdir(targetPath, { withFileTypes: true }, (err, files) => {
+        if (err) return;
+        for (const file of files) {
+          if (!this.#access(file.name) || !file.isDirectory() || !this.#options.deep) continue;
+          this.watch(path.join(targetPath, file.name));
+        }
+      });
     });
   };
 
