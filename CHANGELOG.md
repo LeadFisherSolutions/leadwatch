78b20d2db037a82d892985ebe4f03392cfa3c9a6
Alexander Ivanov <sashapop10@yandex.ru>
2023-06-03 19:36:37 +0300

    1.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,17 @@
 # Changelog
 
+## [1.3.0][] - 2023-06-03
+
+- Before / after events
+- Updated tests
+  1. Ignore case
+  2. Before / after case
+  3. Specific file case
+  4. Deep files case
+- Move private methods to lib folder
+- Move from Watcher class to functional object
+- Fix .d.ts exports
+
 ## [1.2.0][] - 2023-06-02
 
 - package.json files
@@ -14,6 +26,7 @@
 
 - Stable version
 
+[1.3.0]: https://github.com/LeadFisherSolutions/workspace-example/compare/v1.2.0...v1.3.0
 [1.2.0]: https://github.com/LeadFisherSolutions/workspace-example/compare/v1.1.0...v1.2.0
 [1.1.0]: https://github.com/LeadFisherSolutions/workspace-example/compare/release...v1.1.0
 [1.0.0]: https://github.com/LeadFisherSolutions/workspace-example/releases/tag/release
--- index.js
+++ index.js
@@ -1,75 +1,41 @@
 'use strict';
 
 const fs = require('node:fs');
-const path = require('node:path');
+const { join, sep } = require('node:path');
+const { access, schedular } = require('./lib');
 const { EventEmitter } = require('node:events');
-const WATCH_TIMEOUT = 10000;
 
-module.exports = class Watcher extends EventEmitter {
-  #watchers = new Map();
-  #queue = new Map();
-  #timer = null;
-  #timeout;
-  #ignore;
-  #deep;
+module.exports = function (options) {
+  const watchers = new Map();
+  const bridge = new EventEmitter();
+  const post = schedular(options?.timeout, bridge.emit.bind(bridge));
+  const inIgnore = access.bind(null, options?.ignore ?? []);
 
-  constructor(options) {
-    super();
-    this.#deep = options?.deep;
-    this.#timeout = options?.timeout ?? WATCH_TIMEOUT;
-    this.#ignore = options?.ignore ?? [];
-  }
-
-  #access = file => !this.#ignore.reduce((acc, pattern) => (acc |= new RegExp(pattern).test(file)), false);
-  #post = (event, filePath) => {
-    if (this.#timer) clearTimeout(this.#timer);
-    this.#timer = setTimeout(() => this.#sendQueue(), this.#timeout);
-    this.#queue.set(filePath, event);
-  };
-
-  #sendQueue = () => {
-    if (!this.#timer) return;
-    clearTimeout(this.#timer);
-    this.#timer = null;
-    for (const [filePath, event] of this.#queue) this.emit(event, filePath);
-    this.#queue.clear();
-  };
-
-  #setWatcher = targetPath => {
-    if (this.#watchers.has(targetPath)) return;
-    const watcher = fs.watch(targetPath, (_, filename) => {
-      const target = targetPath.endsWith(path.sep + filename);
-      const filePath = target ? targetPath : path.join(targetPath, filename);
-      if (!this.#access(filePath)) return;
-      fs.stat(filePath, (err, stats) => {
-        if (err) return void this.unwatch(filePath), this.#post('delete', filePath);
-        if (stats.isDirectory() && this.#deep) this.watch(filePath);
-        this.#post('change', filePath);
+  const setWatcher = path => {
+    if (watchers.has(path)) return;
+    const listener = (_, filename) => {
+      const eventPath = path.endsWith(sep + filename) ? path : join(path, filename);
+      if (!inIgnore(eventPath)) return;
+      fs.stat(eventPath, (err, stats) => {
+        if (err) return void (bridge.unwatch(eventPath), post('delete', eventPath));
+        stats.isDirectory() && options?.deep && bridge.watch(eventPath), post('change', eventPath);
         return void 0;
       });
-    });
-    this.#watchers.set(targetPath, watcher);
+    };
+    watchers.set(fs.watch(path, listener));
   };
 
-  watch = targetPath => {
-    if (this.#watchers.get(targetPath)) return;
-    fs.stat(targetPath, (err, stats) => {
+  bridge.unwatch = path => void (watchers.get(path)?.close(), watchers.delete(path));
+  bridge.watch = path => {
+    if (watchers.has(path)) return;
+    fs.stat(path, (err, stats) => {
       if (err) return;
-      this.#setWatcher(targetPath);
-      if (!stats.isDirectory()) return;
-      fs.readdir(targetPath, { withFileTypes: true }, (err, files) => {
-        if (err) return;
-        for (const file of files) {
-          if (!this.#access(file.name) || !file.isDirectory() || !this.#deep) continue;
-          this.watch(path.join(targetPath, file.name));
-        }
-      });
+      setWatcher(path);
+      if (!stats.isDirectory() || !options?.deep) return;
+      const cb = files => files.forEach(f => !inIgnore(f.name) && f.isDirectory() && bridge.watch(join(path, f.name)));
+      fs.readdir(path, { withFileTypes: true }, (err, files) => !err && cb(files));
     });
   };
 
-  unwatch = targetPath => {
-    const watcher = this.#watchers.get(targetPath);
-    if (!watcher) return;
-    watcher.close(), this.#watchers.delete(targetPath);
-  };
-}
+  return bridge;
+};
